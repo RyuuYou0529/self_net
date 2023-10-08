60c226351f2cff4d237690e829074424d656ded4
RyuuYou <ryuuyou0529@gmail.com>
2023-10-08 13:31:55 +0800

    update

--- python_code/Train_self_net.py
+++ python_code/Train_self_net.py
@@ -9,6 +9,9 @@ import itertools
 from ssim import SSIM
 from Charbonnier_loss import L1_Charbonnier_loss
 
+import sys
+import time
+from torch.utils.tensorboard import SummaryWriter
 
 def adjust_lr(init_lr,optimizer,epoch,step,gamma):
     if (epoch+1)%step==0:
@@ -51,10 +54,12 @@ def set_requires_grad(net, requires_grad=False):
 def train(train_set,netD_A,netD_B,netG_A,netG_B,deblur_net,args,device,criterionGAN,criterionL1,SSIM_loss,optimD,optimG,optim_deblur):
 
     lambda_cycle = 1
-    lambda_deblur=1
-    lambda_feedback=0
-    lambda_gan=0.1
-    lambda_ssim=0.1
+    lambda_deblur = 1
+    lambda_feedback = 0
+    lambda_gan = 0.1
+    lambda_ssim = 0.1
+
+    lambda_accord = 1
 
 
     netD_A.train()
@@ -64,7 +69,7 @@ def train(train_set,netD_A,netD_B,netG_A,netG_B,deblur_net,args,device,criterion
     netG_B.train()
     deblur_net.train()
 
-    fakeA_pool=pytorch_dataset.ImagePool(50)
+    fakeA_pool = pytorch_dataset.ImagePool(50)
     fakeB_pool = pytorch_dataset.ImagePool(50)
 
     Loss_D=0.0
@@ -75,25 +80,26 @@ def train(train_set,netD_A,netD_B,netG_A,netG_B,deblur_net,args,device,criterion
     Loss_deblur=0.0
 
 
+    writer = SummaryWriter(log_dir=os.path.join(args.path, 'checkpoint/tensorboard'))
+    total_iters= 0
     for epoch in range(args.epochs):
         param1 = optimD.param_groups[0]
         param2=optimG.param_groups[0]
-        adjust_lr(args.learning_rate_D, optimD, epoch,15,0.5)
-        adjust_lr(args.learning_rate_G, optimG, epoch,15,0.5)
-        adjust_lr(args.learning_rate_G, optim_deblur, epoch,15,0.5)
+        adjust_lr(args.learning_rate_D, optimD, epoch, 15, 0.5)
+        adjust_lr(args.learning_rate_G, optimG, epoch, 15, 0.5)
+        adjust_lr(args.learning_rate_G, optim_deblur, epoch, 15, 0.5)
 
-        print('epoch:{},learning_rate_D:{}'.format(epoch+1,param1['lr']))
-        print('epoch:{},learning_rate_G:{}'.format(epoch + 1, param2['lr']))
-
+        print(f'[epoch:{epoch+1}]  learning_rate_D:{param1["lr"]}')
+        print(f'[epoch:{epoch+1}]  learning_rate_G:{param2["lr"]}')
 
         if epoch+1>=2:
             lambda_feedback=0.1
 
-
         for i, data in enumerate(train_set):
+            batch_time_start = time.time()
+            total_iters += args.batch_size
 
             #degradation modeling
-
             A=data['xy_lr']
             B=data['xz']
             C=data['xy']
@@ -102,19 +108,16 @@ def train(train_set,netD_A,netD_B,netG_A,netG_B,deblur_net,args,device,criterion
             B=B.to(device)
             C=C.to(device)
 
-            fake_B=netG_A(A)      #syn_xz
-
+            fake_B=netG_A(A)      #syn_lr
             fake_A=netG_B(B)
 
             set_requires_grad(deblur_net,False)
             fake_C1 = deblur_net(fake_B)
 
             rec_A=netG_B(fake_B)
-
             rec_B=netG_A(fake_A)
 
             #optimize G
-
             set_requires_grad(netD_A, False)
             set_requires_grad(netD_B, False)
 
@@ -122,29 +125,39 @@ def train(train_set,netD_A,netD_B,netG_A,netG_B,deblur_net,args,device,criterion
 
             # GAN loss D_A(G_A(A))
             loss_G_A = lambda_gan*criterionGAN(netD_A(fake_B), True)
+            writer.add_scalar('Loss_G/loss_G_A', loss_G_A, global_step=total_iters)
             # GAN loss D_B(G_B(B))
             loss_G_B = lambda_gan*criterionGAN(netD_B(fake_A), True)
+            writer.add_scalar('Loss_G/loss_G_B', loss_G_B, global_step=total_iters)
 
 
             # Forward cycle loss || G_B(G_A(A)) - A||
-
-
             loss_cycle_A = lambda_cycle*criterionL1(rec_A,A)+lambda_ssim*(1-SSIM_loss(rec_A,A))
-
+            writer.add_scalar('Loss_G/loss_cycle_A', loss_cycle_A, global_step=total_iters)
             # Backward cycle loss || G_A(G_B(B)) - B||
-
             loss_cycle_B = lambda_cycle*criterionL1(rec_B,B)+lambda_ssim*(1-SSIM_loss(rec_B,B))
+            writer.add_scalar('Loss_G/loss_cycle_B', loss_cycle_B, global_step=total_iters)
 
+            # ACCORD Loss
+            accord_B = netG_A(rec_A)
+            accord_A = netG_B(rec_B)
+            loss_accord_G_A = lambda_accord*criterionL1(fake_B, accord_B) + lambda_ssim*(1-SSIM_loss(fake_B, accord_B))
+            loss_accord_G_B = lambda_accord*criterionL1(fake_A, accord_A) + lambda_ssim*(1-SSIM_loss(fake_A, accord_A))
+            writer.add_scalar('Loss_accord/loss_accord_G_A', loss_accord_G_A, global_step=total_iters)
+            writer.add_scalar('Loss_accord/loss_accord_G_B', loss_accord_G_B, global_step=total_iters)
 
             loss_feedback = lambda_feedback*(criterionL1(fake_C1, C)+lambda_ssim*(1-SSIM_loss(fake_C1,C)))
+            writer.add_scalar('Loss_G/loss_feedback', loss_feedback, global_step=total_iters)
 
-            loss_G = loss_G_A + loss_G_B + loss_cycle_A + loss_cycle_B + loss_feedback
+            # loss_G = loss_G_A + loss_G_B + loss_cycle_A + loss_cycle_B + loss_feedback
+            # loss_G = loss_G_A + loss_G_B + loss_feedback + loss_accord_G_A + loss_accord_G_B
+            loss_G = loss_G_A + loss_G_B + loss_cycle_A + loss_cycle_B + loss_feedback + loss_accord_G_A + loss_accord_G_B
+            writer.add_scalar('Loss_G/loss_G', loss_G, global_step=total_iters)
 
             loss_G.backward()
             optimG.step()
 
             #optimize D
-
             if (i+1)%2==0:
                 set_requires_grad(netD_A, True)
                 set_requires_grad(netD_B, True)
@@ -152,62 +165,53 @@ def train(train_set,netD_A,netD_B,netG_A,netG_B,deblur_net,args,device,criterion
                 optimD.zero_grad()
 
                 fake_B = fakeB_pool.query(fake_B)
-
-                loss_D_A =backward_D_basic(lambda_gan,netD_A, criterionGAN, B, fake_B, 'lsgan', device)
-
                 fake_A = fakeA_pool.query(fake_A)
-                loss_D_B = backward_D_basic(lambda_gan,netD_B,criterionGAN,A,fake_A,'lsgan',device)
+                loss_D_A = backward_D_basic(lambda_gan,netD_A, criterionGAN, B, fake_B, 'lsgan', device)
+                loss_D_B = backward_D_basic(lambda_gan,netD_B, criterionGAN, A, fake_A, 'lsgan', device)
 
                 optimD.step()
 
             else:
                 loss_D_A=torch.Tensor([0.0])
                 loss_D_B=torch.Tensor([0.0])
+            writer.add_scalar('Loss_D/loss_D_A', loss_D_A, global_step=total_iters)
+            writer.add_scalar('Loss_D/loss_D_B', loss_D_B, global_step=total_iters)
 
             #Checkpoint
-
-            Loss_D += loss_D_A.item() + loss_D_B.item()
-            Loss_G_GAN+=loss_G_A.item()+loss_G_B.item()
-
-            Loss_feedback+=loss_feedback.item()
-
-            Loss_G_cycle+=loss_cycle_A.item()+loss_cycle_B.item()
+            Loss_D += loss_D_A.item()+loss_D_B.item()
+            Loss_G_GAN += loss_G_A.item()+loss_G_B.item()
+            Loss_feedback += loss_feedback.item()
+            Loss_G_cycle += loss_cycle_A.item()+loss_cycle_B.item()
 
             if (i + 1) % args.log_interval == 0:
-                print('epochs: {} {}/{}  degradation modeling stage: loss_D:{:4f} , '
-                      'loss_G: {:4f} loss_G_cycle:{:4f} loss_feedback:{:4f}'.
-                format(epoch+1,
-                    (i + 1) * args.batch_size, len(train_set.dataset),
-                    Loss_D / (args.log_interval*lambda_gan),Loss_G_GAN/(args.log_interval*lambda_gan),
-                    Loss_G_cycle / (args.log_interval*lambda_cycle),Loss_feedback/(args.log_interval)))
-
+                print(f'[epochs: {epoch+1} {(i+1)*args.batch_size}/{len(train_set.dataset)}] [degradation modeling stage]: '
+                      f'loss_D:{Loss_D/(args.log_interval*lambda_gan):4f}, '
+                      f'loss_G: {Loss_G_GAN/(args.log_interval*lambda_gan):4f}, '
+                      f'loss_G_cycle:{Loss_G_cycle/(args.log_interval*lambda_cycle):4f}, '
+                      f'loss_feedback:{Loss_feedback/(args.log_interval):4f}')
                 Loss_D = 0.0
                 Loss_G_GAN = 0.0
                 Loss_G_cycle = 0.0
                 Loss_feedback=0.0
 
             #deblurring
-
             set_requires_grad(deblur_net, True)
-            fake_B = netG_A(A)  # syn_lr_xy
+            fake_B = netG_A(A)  # syn_hr_deg
             fake_C2 = deblur_net(fake_B.detach())
 
             optim_deblur.zero_grad()
 
             loss_deblur = lambda_deblur * criterionL1(fake_C2, C) + lambda_ssim * (1 - SSIM_loss(fake_C2, C))
+            writer.add_scalar('Loss_Deblur/loss_deblur', loss_deblur, global_step=total_iters)
 
             loss_deblur.backward()
             optim_deblur.step()
 
             # Checkpoint
-
             Loss_deblur += loss_deblur.item()
 
             if (i + 1) % args.log_interval == 0:
-                print('epochs: {} {}/{}  deblurring stage :loss_deblur:{:4f}'.
-                      format(epoch + 1,
-                             (i + 1) * args.batch_size, len(train_set.dataset),Loss_deblur /(args.log_interval*lambda_deblur)))
-
+                print(f'[epochs: {epoch+1} {(i+1)*args.batch_size}/{len(train_set.dataset)}]  [deblurring stage]: loss_deblur:{Loss_deblur /(args.log_interval*lambda_deblur):4f}')
                 Loss_deblur = 0.0
 
             if (i + 1) % args.imshow_interval == 0:
@@ -217,15 +221,19 @@ def train(train_set,netD_A,netD_B,netG_A,netG_B,deblur_net,args,device,criterion
                 torch.save(netG_B.state_dict(),os.path.join(args.path, 'checkpoint/saved_models/', 'netG_B/', f'{epoch + 1}_{(i+1)*args.batch_size}.pkl'))
                 torch.save(netD_A.state_dict(),os.path.join(args.path, 'checkpoint/saved_models/', 'netD_A/', f'{epoch + 1}_{(i+1)*args.batch_size}.pkl'))
                 torch.save(netD_B.state_dict(),os.path.join(args.path, 'checkpoint/saved_models/', 'netD_B/', f'{epoch + 1}_{(i+1)*args.batch_size}.pkl'))
+            
+            if (i + 1) % args.log_interval == 0:
+                batch_time_end = time.time()
+                print(f'[time]: {batch_time_end-batch_time_start :.6f} \n')
                 
 
 
 
 def main():
     parser=argparse.ArgumentParser(description='Self_Net')
-    parser.add_argument('--batch_size',type=int,default=16)
+    parser.add_argument('--batch_size',type=int,default=32)
     parser.add_argument('--epochs',type=int,default=60)
-    parser.add_argument('--path', default='/home/ryuuyou/Project/self_net/data/care_liver/')
+    parser.add_argument('--path', default='/home/ryuuyou/Project/self_net/data/care_liver')
 
     parser.add_argument('--min_v', type=int, default=0)
     parser.add_argument('--max_v', type=int, default=1)
@@ -264,8 +272,6 @@ def main():
     if not os.path.exists(intermediate_results_path):
         os.mkdir(intermediate_results_path)
 
-
-
     device=torch.device('cuda:0')
 
     input_nc=1
@@ -295,4 +301,10 @@ def main():
 
 
 if __name__=='__main__':
+    f = open('train.log', 'w')
+    sys.stdout = f
+    sys.stderr = f
+
     main()
+
+    f.close()
